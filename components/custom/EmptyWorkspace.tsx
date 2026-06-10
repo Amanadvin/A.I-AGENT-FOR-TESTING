b8cbe850e59c670c5fbdebf60a4f96491f46dab7
affan <affanalam2019>
2026-06-10 22:17:39 +0530

    Done

--- components/custom/EmptyWorkspace.tsx
+++ components/custom/EmptyWorkspace.tsx
@@ -1,38 +1,36 @@
-import Image from 'next/image'
-import React from 'react'
-import { Button } from '../ui/button'
-import { Link } from 'lucide-react'
+import Image from "next/image";
+import React from "react";
+import { Button } from "../ui/button";
+import { Link } from "lucide-react";
 
 function EmptyWorkspace() {
-    return (
-        <div className='flex flex-col mt-10 items-center justify-center'>
-<<<<<<< HEAD
-            <Image src={'/folder.png'} alt='folder' width={70} height={70} />
-            <h2 className='font-medium text-2xl mt-5 mb-4'>No Repository Connected</h2>
-            <p className='text-center mx-10'>Connect your Github accounts and add a repository to generate and run test cases.</p>
-=======
-            <Image
-                src={'/folder.png'}
-                alt='folder'
-                width={70}
-                height={70}
-            />
->>>>>>> 70edb293c4afc6dd2578c883806238ca625480cf
+  return (
+    <div className="flex flex-col mt-10 items-center justify-center">
+      <Image src={"/folder.png"} alt="folder" width={70} height={70} />
+      <h2 className="font-medium text-2xl mt-5 mb-4">
+        No Repository Connected
+      </h2>
+      <p className="text-center mx-10">
+        Connect your Github accounts and add a repository to generate and run
+        test cases.
+      </p>
+      <Image src={"/folder.png"} alt="folder" width={70} height={70} />
 
-            <h2 className='font-medium text-2xl mt-5 mb-4'>
-                No Repository Connected
-            </h2>
+      <h2 className="font-medium text-2xl mt-5 mb-4">
+        No Repository Connected
+      </h2>
 
-            <p className='text-center mx-10'>
-                Connect your Github accounts and add a repository to generate and run test cases
-            </p>
+      <p className="text-center mx-10">
+        Connect your Github accounts and add a repository to generate and run
+        test cases
+      </p>
 
-            <Button className='mt-5'>
-                <Link className='h-4 w-4 mr-2' />
-                Connect Repo
-            </Button>
-        </div>
-    )
+      <Button className="mt-5">
+        <Link className="h-4 w-4 mr-2" />
+        Connect Repo
+      </Button>
+    </div>
+  );
 }
 
-export default EmptyWorkspace
+export default EmptyWorkspace;
